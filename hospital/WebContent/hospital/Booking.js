3b629f1e14f4ae4eae4f3dd71bbe44e0425735a7
ninefox1217 <ninefox1217@naver.com>
2017-07-26 15:36:24 +0900

    수정 1차

--- hospital/WebContent/hospital/Booking.js
+++ hospital/WebContent/hospital/Booking.js
@@ -8,4 +8,23 @@ bookingDate = bookingDate;//예약 날짜
 doctorNo = doctorNo;//의사번호
 petientNo=petientNo;//환자번호
 
-} 
+}
+
+
+Booking.prototype.getBookingNo = function(){return this.bookingNo;}
+Booking.prototype.setBookingNo = function(bookingNo){this.bookingNo = bookingNo;}
+//------------------------------------------------------
+Booking.prototype.getBookingDate = function(){return this.bookingDate;}
+Booking.prototype.setBookingDate = function(bookingDate){this.bookingDate = bookingDate;}
+//------------------------------------------------------
+Booking.prototype.getDoctorNo = function(){return this.doctorNo;}
+Booking.prototype.setDoctorNo = function(doctorNo){this.doctorNo = doctorNo;}
+//------------------------------------------------------
+Booking.prototype.getPetientNo = function(){return this.petientNo;}
+Booking.prototype.setPetientNo = function(petientNo){this.petientNo = petientNo;}
+//------------------------------------------------------
+Booking.prototype.toString = function(){return 'bookingNo : ' + this.bookingNo +
+											   'bookingDate : ' + this.bookingDate + 
+											   'doctorNo : ' + this.doctorNo +
+											   'petientNo : ' + this.petientNo ;
+}
--- hospital/WebContent/hospital/petient.js
+++ hospital/WebContent/hospital/petient.js
@@ -2,12 +2,38 @@
  * 환자
 */
 
-function Petient(petientNo, petientName, DisName, ssNumber, phoneNumber, address){
+function Petient(petientNo, petientName, disName, ssNumber, phoneNumber, address){
 petientNo = petientNo;//환자번호
 petientName = petientName;//환자이름
-DisName = DisName;//병명
+disName = disName;//병명
 ssNumber = ssNumber;//주민번호
 phoneNumber = phoneNumber;//전화번호
 address = address;//주소
 
+}
+
+Petient.prototype.getPetientNo = function(){return this.petientNo;}
+Petient.prototype.setPetientNo = function(petientNo){this.petientNo = petientNo;}
+//------------------------------------------------------
+Petient.prototype.getPetientName = function(){return this.petientName;}
+Petient.prototype.setPetientName = function(petientName){this.petientName = petientName;}
+//------------------------------------------------------
+Petient.prototype.getDisName = function(){return this.disName;}
+Petient.prototype.setDisName = function(disName){this.disName = disName;}
+//------------------------------------------------------
+Petient.prototype.getSsNumber = function(){return this.ssNumber;}
+Petient.prototype.setSsNumber = function(ssNumber){this.ssNumber = ssNumber;}
+//------------------------------------------------------
+Petient.prototype.getPhoneNumber = function(){return this.phoneNumber;}
+Petient.prototype.setPhoneNumber = function(phoneNumber){this.phoneNumber = phoneNumber;}
+//------------------------------------------------------
+Petient.prototype.getAddress = function(){return this.address;}
+Petient.prototype.setAddress = function(address){this.address = address;}
+//------------------------------------------------------
+Petient.prototype.toString = function(){return 'petientNo : ' + this.petientNo +
+											   'petientName : ' + this.petientName + 
+											   'disName : ' + this.disName +
+											   'ssNumber : ' + this.ssNumber +
+											   'phoneNumber : ' + this.phoneNumber +
+											   'address : ' + this.address ;
 }
